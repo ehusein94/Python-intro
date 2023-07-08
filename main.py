f363d4e721bd757d68b547b9629f84adc8245d78
asalhieh <salhiehanmar2@gmail.com>
2023-07-08 11:58:53 +0300

    Learn about index and slice.

---
+++ main.py
@@ -0,0 +1,17 @@
+var1="value"
+print(var1)
+var2=7
+var3=3.14
+var4=False
+var5= 5+ 6j
+# print (var1, var2,var3,var4,var5)
+# print(type (var1) , type (var2),type (var3),type (var4), type (var5))
+# print ( var1[-1])
+# print(var1[1:])
+# print (len(var1))
+str1='Hello'
+print (str1)
+str2='y'+str1[1:len(str1)]
+print (str2)
+str1=str1[0:2]+'MM'+str1[-1:]
+print (str1)
